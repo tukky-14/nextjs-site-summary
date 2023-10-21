2c42bd4fefe9424a73a2c39c32798302c0b669a6
tukky-14 <bigoonage0104@gmail.com>
2023-10-21 22:44:50 +0900

    Clearボタン追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,6 +18,11 @@ export default function Home() {
         dangerouslyAllowBrowser: true,
     });
 
+    const clearUrl = () => {
+        setUrl('');
+        setSummary('');
+    };
+
     const fetchData = async () => {
         try {
             setSummary('');
@@ -91,13 +96,22 @@ export default function Home() {
                             required
                         />
                     </div>
-                    <button
-                        className="w-40 py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                        disabled={!url || loading}
-                        onClick={fetchData}
-                    >
-                        {loading ? 'Loading...' : 'Submit'}
-                    </button>
+                    <div className="flex gap-4">
+                        <button
+                            className="w-40 py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                            disabled={!url || loading}
+                            onClick={fetchData}
+                        >
+                            {loading ? 'Loading...' : 'Submit'}
+                        </button>
+                        <button
+                            className="w-40 py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                            disabled={loading}
+                            onClick={clearUrl}
+                        >
+                            Clear
+                        </button>
+                    </div>
                 </div>
                 <div className="dark:text-white">
                     <h2 className="mb-2">Summary</h2>
