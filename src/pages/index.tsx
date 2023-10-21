109d61659e78cffe2cf56b798b4b99529d72c964
tukky-14 <bigoonage0104@gmail.com>
2023-10-21 11:04:07 +0900

    TailwindCSS設定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
-import Head from "next/head";
+import Head from 'next/head';
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Site Summary</title>
-        <meta name="description" content="Site Summary" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main></main>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>Site Summary</title>
+                <meta name="description" content="Site Summary" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main></main>
+        </>
+    );
 }
