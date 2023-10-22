2fbf0390be7be13511c949ebc397a0796465cf54
tukky-14 <bigoonage0104@gmail.com>
2023-10-22 22:51:24 +0900

    URLのinput要素を選択したら文字全体が選択されるように更新

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -96,6 +96,9 @@ export default function Home() {
                             id="first_name"
                             value={url}
                             onChange={(e) => setUrl(e.target.value)}
+                            onClick={(e: React.MouseEvent<HTMLInputElement>) =>
+                                e.currentTarget.select()
+                            }
                             placeholder="Enter URL"
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             required
