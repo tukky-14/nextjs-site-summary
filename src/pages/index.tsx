e4c2bcab62165b16eb67ccf2cc9347f481d0b93d
tukky-14 <bigoonage0104@gmail.com>
2023-10-26 18:41:20 +0900

    Loading中のモーダル表示を追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -76,7 +76,6 @@ export default function Home() {
             });
             setSummary(chatCompletion.choices[0]?.message.content || '');
         } catch (error) {
-            setLoading(false);
             alert('Error fetching summary');
             console.error('Error fetching summary:', error);
         }
@@ -123,7 +122,7 @@ export default function Home() {
                             disabled={!url || loading}
                             onClick={fetchData}
                         >
-                            {loading ? 'Loading...' : 'Submit'}
+                            Submit
                         </button>
                         <button
                             className="w-40 py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
@@ -134,6 +133,34 @@ export default function Home() {
                         </button>
                     </div>
                 </div>
+                {loading && (
+                    <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-50">
+                        <div className="flex flex-col items-center space-y-3 p-6 bg-white rounded-lg shadow-xl">
+                            <svg
+                                className="animate-spin h-5 w-5 text-blue-500"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                            >
+                                <circle
+                                    className="opacity-25"
+                                    cx="12"
+                                    cy="12"
+                                    r="10"
+                                    stroke="currentColor"
+                                    strokeWidth="4"
+                                ></circle>
+                                <path
+                                    className="opacity-75"
+                                    fill="currentColor"
+                                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                ></path>
+                            </svg>
+
+                            <span className="text-xs">Loading...</span>
+                        </div>
+                    </div>
+                )}
                 <div className="dark:text-white">
                     <h2 className="mb-2">Summary</h2>
                     <div
